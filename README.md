55618105fc4ea7ecf2891e6d0de39ac45d68f794
Murray Patterson <murray.patterson@gmail.com>
2020-12-15 19:26:24 -0500

    adapt to iCollege

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # Grading
 
-Final grades were calculated by downloading the grades data from Blackboard in comma-separated values (CSV) format, and running the `calculate-grade.py` python script on it --- not unlike the course project of CS 131 (but in python).  The "Feedback to Learner" along with your Final Grade on Blackboard are the lines of this CSV file pertaining to your grades: see `input.csv` for an example of what this should look like
+Final grades were calculated by downloading the grades data from
+iCollege in comma-separated values (CSV) format --- see `input.csv`
+for an example of what the grades data should look like --- and
+running the `calculate-grade.py` python script on it
 
 ## Calculate your own grade
 
 [![Run on Repl.it](https://repl.it/badge/github/murraypatterson/grading)](https://repl.it/github/murraypatterson/grading)
 
-Running the `calculate-grade.py` python script on `input.csv` can be done in "repl.it" by clicking on the above `run on repl.it` badge: it will load a "repl" where you can click the "run" button to see the result.  To calculate your own grade, you can first replace `input.csv` with the contents of your "Feedback to Learner" from Blackboard: a simple copy of the text of your "Feedback to Learner" and paste within the `input.csv` file inside the repl (replacing the original contents of `input.csv`) is easiest.  After this is done, you can click the "run" button again: the output should match that of your "Grading Notes" along with your Final Grade on Blackboard
+Running the `calculate-grade.py` python script on `input.csv` can be
+done in "repl.it" by clicking on the above `run on repl.it` badge: it
+will load a "repl" where you can click the "run" button to see the
+result.  To calculate your own grade, you can first replace
+`input.csv` with the contents of your grades from iCollege.  After
+this is done, you can click the "run" button again: the output should
+match that of your "Final Adjusted Grade" on iCollege
 
 ## Advanced usage
 
 The precise command that is run when the "run" button is clicked is:
 
-    python3 calculate-grade.py -g groups_131.txt -l fairfield_scheme.txt input.csv 
+    python3 calculate-grade.py -g groups_4330.txt input.csv 
 
-where `groups_131.txt` is the grading scheme of your particular course, and `fairfield_scheme.txt` is the correspondence between numerical and letter grades for your particular university (Fairfield U. in this case).  The behavior of the "run" button is dictated by what is in the `.replit` file.  This can be customized by changing the grading scheme, _e.g._, to `groups_354.txt`, _etc._
+where `groups_4330.txt` is the grading scheme of your particular
+course.  The behavior of the "run" button is dictated by what is in
+the `.replit` file.
--- calculate-grade.py
+++ calculate-grade.py
@@ -14,20 +14,20 @@ import argparse
       header column two
       ...
 
-      # homework 0.3
-      Homework Week 1 [Total Pts: 100 Score] |...
+      # homework 0.5
+      Homework 1 Points Grade <Numeric MaxPoints:100>
       ...
 
-      # midterm 0.2
-      Midterm [Total Pts: 100 Score] |...
+      # midterm 0.25
+      Midterm Exam Points Grade <Numeric MaxPoint:100>
    ```
 
    where the first line starts with `##` and then contains some header
    columns (which do not count).  Then, the remaining "groups",
    starting with `#`, indicate groups of columns (e.g., "homework")
    which group together a set of items, each of which together have
-   some weight towards the total grade for the course (e.g., 0.3, or
-   30% of the grade comes from homework)
+   some weight towards the total grade for the course (e.g., 0.5, or
+   50% of the grade comes from homework)
 
    Note that the set of columns comes from the csv, and so the
    expected way to get this groups file is to run
@@ -41,20 +41,20 @@ import argparse
    It is important to do it this way (not only because there a sanity
    check to ensure the columns of the group match that of the input
    csv file, but) because another thing which is done is the
-   collection of scores for each column (e.g., [Total Pts: 100
-   Score]).  At the moment, the actual score of each column is just
-   normalized by this score, averaged, and then weighted by the group
-   weight, i.e., each column contributes equally to the group.  If we
-   wanted otherwise, the most general solution would be to have a set
-   of column-specific weights, so that we would be taking a weighted
-   average rather than weighting an average afterwords --- could be a
-   solution for the future
+   collection of scores for each column (e.g., <Numeric
+   MaxPoints:100>).  At the moment, the actual score of each column is
+   just normalized by this score, averaged, and then weighted by the
+   group weight, i.e., each column contributes equally to the group.
+   If we wanted otherwise, the most general solution would be to have
+   a set of column-specific weights, so that we would be taking a
+   weighted average rather than weighting an average afterwords ---
+   could be a solution for the future
 
 '''
 def process_groups(lines) :
 
     group = None
-    groups = {}
+    members = {} # members of a group
     weight = {} # weight of a group
     universe = set([]) # universal set of columns
     score = {} # score for individual column
@@ -71,7 +71,7 @@ def process_groups(lines) :
             assert not group
             group = 'header'
 
-            groups[group] = []
+            members[group] = []
             weight[group] = float(0)
 
             continue
@@ -80,77 +80,31 @@ def process_groups(lines) :
         if line.startswith('#') :
 
             # set current group (weight)
-            h, group, w = line.split()
+            _, group, w = line.split()
 
-            groups[group] = []
+            members[group] = []
             weight[group] = float(w)
 
         # column of (the current) group
         else :
 
             column = line.strip()
-            for sep in ['Score]', 'Percentage]'] :
-                if sep in column :
-                    column = column.split(sep,1)[0] + sep
-
             universe.add(column)
 
             assert group
-            groups[group].append(column)
+            members[group].append(column)
 
             if group == 'header' :
                 continue
 
             # non-header group stuff..
-            aux = column.split('[Total Pts:',1)[1]
-            score[column] = float(aux.split('Score]',1)[0].strip())
+            _, aux = column.split('<Numeric MaxPoints:', 1)
+            score[column] = float(aux.rstrip('>'))
 
     # weights should add up to 1
-    regular = groups.keys() - ['bonus'] # exclude special "bonus" group
-    assert round(sum(weight[group] for group in regular), 6) == float(1)
+    assert round(sum(weight[g] for g in members), 6) == float(1)
 
-    return universe, groups, weight, score
-
-#
-# process a letter grading scheme from a scheme file
-def process_scheme(lines) :
-
-    letter = {}
-    gpa = {}
-    for line in lines :
-
-        # skip empty lines or "comment" lines
-        if not line.strip() :
-            continue
-
-        if line.startswith('#') :
-            continue
-
-        # A 4 93
-        letter_grade, gpa_grade, grade = line.split()
-        grade = float(grade)
-
-        assert grade >= float(0)
-        assert grade <= float(100)
-
-        letter[grade] = letter_grade
-        gpa[grade] = float(gpa_grade)
-
-    return letter, gpa
-
-#
-# return a letter grade, given a grade and a letter grading scheme
-def letter_grade(letter, grade) :
-
-    indices = sorted(letter.keys())
-    index = indices[0]
-    for i in indices :
-
-        if grade < i :
-            break
-        index = i
-
-    return letter[index]
+    return universe, members, weight, score
 
 #
 # Parser
@@ -158,7 +112,7 @@ def letter_grade(letter, grade) :
 
 parser = argparse.ArgumentParser(description = '''
 
-   Do various things with a csv file downloaded from Blackboard,
+   Do various things with a csv file downloaded from iCollege,
    essentially inferring the final grade by aggregating weighted
    groups of columns.  Output is a table with the weighted groups, and
    the final grade to stdout
@@ -168,31 +122,25 @@ parser = argparse.ArgumentParser(description = '''
    Note: that the output could be generalized to a choice of groups,
    or even better, output a csv file with the (added) groups.
 
-   Also note: for some reason, the first character (an underscore-type
-   character) of the csv that Blackboard dumps causes ascii decoding
-   issues --- the current work-around is to open up this csv file and
-   delete this character.  This should be fixed in the future
-
 ''')
 
 parser.add_argument(
     'file', metavar = 'file.csv',
     type = argparse.FileType('r'), default = sys.stdin,
     help = 'input (csv) file (default: stdin)')
+
 parser.add_argument(
     '-c', '--columns',
     action = 'store_true',
     help = 'dump column names of csv and exit')
+
 parser.add_argument(
     '-g', '--groups', metavar = 'groups.txt',
     type = argparse.FileType('r'),
     help = 'file containing the weighted groups')
+
 parser.add_argument(
-    '-l', '--letter', metavar = 'scheme.txt',
-    type = argparse.FileType('r'),
-    help = 'add column of letter grades according to scheme.txt')
-parser.add_argument(
-    '-t', '--tabseparated',
+    '-t', '--tabs',
     action = 'store_true',
     help = 'output in tab-separated format (default is CSV)')
 
@@ -202,107 +150,80 @@ args = parser.parse_args()
 # Main
 #----------------------------------------------------------------------
 
-# process the groups, if applicable
-universe, groups, weight, score = None, None, None, None
-columns = []
-if args.groups :
-
-    universe, groups, weight, score = process_groups(args.groups)
-
-    # gather groups we want to report (i.e., those which carry weight)
-    for group in groups :
-        if weight[group] :
-            columns.append(group)
-
-    # print header
-    sep = '\t' if args.tabseparated else ','
-    print('First', 'Last', sep = sep, end = sep)
-    if args.letter :
-        print('Letter', end = sep)
-    print('Grade', 'Course', sep = sep, end = sep)
-    print(*('{:s}({:.3f})'.format(col, weight[col]) for col in columns), sep = sep)
-
-# process letter grading scheme, if applicable
-letter = None
-gpa = None
-if args.letter :
-    letter, gpa = process_scheme(args.letter)
-
 # read in csv as a dictionary
 rows = csv.DictReader(args.file)
+columns = rows.fieldnames
 
-raw_columns = {}
-count = 0
+# dump column names of the header and exit
+if args.columns :
+
+    for column in columns :
+        print(column)
+
+    sys.exit(0)
+
+# process the groups, if applicable
+universe, members, weight, score = None, None, None, None
+groups = []
+if args.groups :
+
+    universe, members, weight, score = process_groups(args.groups)
+
+    # gather groups we want to report (i.e., those which carry weight)
+    for group in members :
+        if weight[group] :
+            groups.append(group)
+
+    u = set([]) # for sanity check against universe
+    for column in columns :
+        u.add(column.strip())
+
+    assert u == universe, universe ^ u
+
+# w\o groups, no need to proceed..
+else :
+    sys.exit(0)
+
+# now we process the rows
+output = {}
 for row in rows :
+    last = row['Last Name'] # output indexed by last name
+    a = [row['First Name']]
 
-    # first row, do some prep
-    if not count :
-
-        if args.columns :
-
-            # dump column names of the header and exit
-            for column in row :
-                print(column)
-
-            sys.exit(0)
-
-        if args.groups :
-
-            # gather raw columns dict, check that columns match groups
-            for column in row :
-
-                col = column.strip()
-                for sep in ['Score]', 'Percentage]'] :
-                    if sep in col :
-                        col = col.split(sep,1)[0] + sep
-
-                raw_columns[col] = column
-
-            assert raw_columns.keys() == universe, universe ^ raw_columns.keys()
-
-        # no need to proceed w\o the groups..
-        else :
-            sys.exit(0)
-
-    # now move on with the student profile
     course = float(0)
-    for group in groups :
+    for group in members :
         if group == 'header' : # blow by the header
             continue
 
         total = float(0)
-        for column in groups[group] :
-
+        for column in members[group] :
             grade = float(0)
-            raw = row[raw_columns[column]]
 
-            if raw == 'Needs Grading' : # just a warning here
-                print('warning: a column needs grading', file = sys.stderr)
-
-            if raw.startswith('In Progress') :
-                print('warning: a column is in progress', file = sys.stderr)
-
-                raw = raw.strip('In Progress')
-                raw = raw.lstrip('(').rstrip(')') # 2nd attempt in progress
-
-            if raw : # column is "" (not submitted)
-                grade = float(raw)
+            g = row[column]
+            if g : # column is not "" (not submitted)
+                grade = float(g)
 
             total += grade / score[column] # add normalized score
 
         # take average, weight it, and add it to final course grade
-        row[group] = total * weight[group] / len(groups[group])
+        row[group] = total * weight[group] / len(members[group])
         course += row[group]
 
-    row['course'] = course
-    grade = round(float(100) * row['course'])
+    # prepare remainder of the row of output
+    a.append(round(float(100) * course))
+    a.append('{:.6f}'.format(course))
 
-    # print out student profile in terms of groups
-    sep = '\t' if args.tabseparated else ','
-    print(row['First Name'], row['Last Name'], sep = sep, end = sep)
-    if args.letter :
-        print(letter_grade(letter, float(grade)), end = sep)
-    print(grade, '{:.6f}'.format(row['course']), sep = sep, end = sep)
-    print(*('{:.6f}'.format(row[col]) for col in columns), sep = sep)
+    for group in groups :
+        a.append('{:.6f}'.format(row[group]))
 
-    count += 1
+    output[last] = a
+
+# print header
+sep = '\t' if args.tabs else ','
+print('Last', 'First', sep = sep, end = sep)
+print('Grade', 'Course', sep = sep, end = sep)
+print(*('{:s}({:.3f})'.format(g, weight[g]) for g in groups), sep = sep)
+
+# output
+for last in sorted(output) :
+    print(last, *output[last], sep = sep)
